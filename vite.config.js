0153a6eadfdd43d07a98c84f2eb49fd66e949792
Shashankteh <kana8432474451@Gmail.com>
2026-03-28 20:24:37 +0530

    vite build fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // Match your GitHub Pages repo name exactly
   base: '/fika-spot-website/',
-});
+})
